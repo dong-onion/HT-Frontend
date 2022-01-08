343969553d5af7029af9e0f85dbde76688e7ee6a
lwamuhaji <lwamuhaji@gmail.com>
2022-01-08 17:39:46 +0900

    회원가입 페이지 구현

--- components/ProfileImage.js
+++ components/ProfileImage.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const ProfileImage = () => {
-  return <View style={styles.block} />;
+  return (
+    <View style={styles.block}>
+      <Icon style={styles.icon} name="camera-enhance" size={30} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -11,7 +16,12 @@ const styles = StyleSheet.create({
     height: 100,
     width: 100,
     borderWidth: 1,
+    borderColor: '#aaaaaa',
+    backgroundColor: '#cccccc',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
   },
+  icon: { padding: 3 },
 });
 
 export default ProfileImage;
