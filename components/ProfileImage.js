855f60b4ff74db6610bd0011ac2a67dd7543ee2c
lwamuhaji <lwamuhaji@gmail.com>
2022-01-10 17:27:06 +0900

    회원가입 UI 및 갤러리 사진 업로드 기능

--- components/ProfileImage.js
+++ components/ProfileImage.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+/* 1월 7일 허준서
+프로필 이미지 컴포넌트입니다.
+react-native-vector-icons,
+react-native-image-picker 라이브러리가 사용되었습니다. */
+
+import React, { useState } from 'react';
+import { Image, Platform, StyleSheet, Pressable, View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { launchImageLibrary } from 'react-native-image-picker';
 
 const ProfileImage = () => {
+  const [response, setResponse] = useState(null);
+
+  const onPress = () => {
+    launchImageLibrary(
+      {
+        mediaType: 'photo',
+        maxWidth: 512,
+        maxHeight: 512,
+        includeBase64: Platform.OS === 'android',
+      },
+      (res) => {
+        if (res.didCancel) {
+          return;
+        }
+        setResponse(res);
+      }
+    );
+  };
+
   return (
-    <View style={styles.block}>
-      <Icon style={styles.icon} name="camera-enhance" size={30} />
-    </View>
+    <Pressable onPress={onPress}>
+      {response === null ? (
+        <View style={styles.block}>
+          <Icon style={styles.icon} name="camera-enhance" size={30} />
+        </View>
+      ) : (
+        <Image
+          style={styles.block}
+          source={{ uri: response?.assets[0]?.uri }}
+        />
+      )}
+    </Pressable>
   );
 };
 
