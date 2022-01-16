114e091d9903434da87d8b2bbe912ad53b016890
960817 <92906766+960817@users.noreply.github.com>
2022-01-16 22:15:47 +0900

    Delete profile screen in SignUpStack

--- screens/SignUpStack.js
+++ screens/SignUpStack.js
@@ -8,8 +8,6 @@ import SignUpScreen1 from './SignUpScreen1';
 import SignUpScreen2 from './SignUpScreen2';
 import { SignUpContextProvider } from '../components/SignUpContext';
 import ProfileImage from '../components/ProfileImage';
-import ProfileScreen from './ProfileScreen';
-import MyProfileScreen from './MyProfileScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -28,18 +26,6 @@ const SignUpStack = () => {
           options={{ title: '회원가입' }}
         />
         <Stack.Screen name="ProfileImg" component={ProfileImage} />
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            title: '프로필',
-          }}
-        />
-        <Stack.Screen
-          name="MyProfile"
-          component={MyProfileScreen}
-          options={{ title: '내 프로필' }}
-        />
       </Stack.Navigator>
     </SignUpContextProvider>
   );
