8cbdec4b966aeb20f234635aedcc595300fa1e6a
lwamuhaji <lwamuhaji@gmail.com>
2022-01-20 17:43:57 +0900

    Add: redux store 생성

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import RootStack from './screens/RootStack';
+import rootReducer from './redux_modules';
+import { createStore } from 'redux';
+
+const store = createStore(rootReducer);
+console.log(store.getState());
 
 const App = () => {
   return (
