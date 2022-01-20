b4d716222aa12b36ad2de15c48910b5c806596c4
lwamuhaji <lwamuhaji@gmail.com>
2022-01-20 17:49:46 +0900

    Add: redux store 적용

--- App.js
+++ App.js
@@ -4,15 +4,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import RootStack from './screens/RootStack';
 import rootReducer from './redux_modules';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 const store = createStore(rootReducer);
 console.log(store.getState());
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
