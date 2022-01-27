8e173055022be90f55b49a555fe06f3ced68df33
lwamuhaji <lwamuhaji@gmail.com>
2022-01-27 21:52:23 +0900

    Add: 서브 리듀서 추가

--- redux_modules/index.js
+++ redux_modules/index.js
@@ -1,8 +1,10 @@
 // 루트 리듀서입니다.
 
 import { combineReducers } from 'redux';
-import signUpInfo from './signUp';
+import signUp from './signUp';
+import signUpAPI from './signUpAPI';
+import profileImage from './profileImage';
 
-const rootReducer = combineReducers({ signUpInfo });
+const rootReducer = combineReducers({ signUp, signUpAPI, profileImage });
 
 export default rootReducer;
