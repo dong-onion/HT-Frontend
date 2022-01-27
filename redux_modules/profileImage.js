8bfd601ba53f7ed32962b3688463c5c96ec5b142
lwamuhaji <lwamuhaji@gmail.com>
2022-01-27 21:52:46 +0900

    Add: 프로필 이미지 리덕스 모듈

---
+++ redux_modules/profileImage.js
@@ -0,0 +1,20 @@
+// 프로필 이미지를 위한 리덕스 모듈
+
+const SET_IMAGE = 'profileImage/SET_IMAGE';
+
+export const setImage = (image) => ({ type: SET_IMAGE, image });
+
+const initialState = {
+  image: null,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_IMAGE:
+      return { image: action.image };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
