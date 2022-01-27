9fbb5e3e48847f3f9d12c7b5bfcdc4f43ff7825b
lwamuhaji <lwamuhaji@gmail.com>
2022-01-27 22:43:26 +0900

    Add: 회원가입 화면 예외처리/리덕스 추가

--- screens/SignUpScreen1.js
+++ screens/SignUpScreen1.js
@@ -1,13 +1,13 @@
 /* 1월 7일 허준서
-회원가입 첫번쨰 화면입니다. */
+회원가입 첫번째 화면입니다. */
 
 import React, { useRef, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import MaterialButton from '../components/MaterialButton';
 import { useNavigation } from '@react-navigation/native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { useSelector, useDispatch } from 'react-redux';
-import { setEmail, setPassword } from '../redux_modules/signup';
+import { useDispatch } from 'react-redux';
+import { setEmail, setPassword } from '../redux_modules/signUp';
 import { TextField, FilledTextField } from 'rn-material-ui-textfield';
 
 const SignUpScreen = () => {
@@ -15,25 +15,38 @@ const SignUpScreen = () => {
   const [localEmail, setLocalEmail] = useState('');
   const [localPassword, setLocalPassword] = useState('');
   const [localPasswordRetype, setLocalPasswordRetype] = useState('');
-  const [error, setError] = useState('');
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+    passwordRetype: '',
+  });
 
   const emailField = useRef();
   const passwordField = useRef();
   const passwordRetype = useRef();
 
   const dispatch = useDispatch();
-  const signupState = useSelector((state) => state.signup);
 
   const onPress = () => {
-    if (localPassword === localPasswordRetype) {
-      setError('');
-
+    // 이메일 유효성 확인
+    if (!localEmail) {
+      const e = { ...errors, email: '이메일을 입력해주세요.' };
+      setErrors(e);
+    }
+    // 비밀번호 유효성 확인
+    else if (!localPassword) {
+      const e = { ...errors, password: '비밀번호를 입력해주세요.' };
+      setErrors(e);
+    }
+    // 비밀번호 재입력 확인
+    else if (localPassword !== localPasswordRetype) {
+      const e = { ...errors, passwordRetype: '비밀번호가 일치하지 않습니다.' };
+      setErrors(e);
+    } else {
+      // 다음화면으로
       dispatch(setEmail(localEmail));
       dispatch(setPassword(localPassword));
-      console.log(signupState);
       navigation.navigate('SignUpScreen-Second');
-    } else {
-      setError('비밀번호가 일치하지 않습니다.');
     }
   };
 
@@ -45,16 +58,25 @@ const SignUpScreen = () => {
     passwordRetype.current.focus();
   };
 
-  const onChangeEmail = (text) => {
-    setLocalEmail(text);
+  const onFocusEmail = () => {
+    setErrors({
+      ...errors,
+      email: '',
+    });
   };
 
-  const onChangePassword = (text) => {
-    setLocalPassword(text);
+  const onFocusPassword = () => {
+    setErrors({
+      ...errors,
+      password: '',
+    });
   };
 
-  const onChangePasswordRetype = (text) => {
-    setLocalPasswordRetype(text);
+  const onFocusPasswordRetype = () => {
+    setErrors({
+      ...errors,
+      passwordRetype: '',
+    });
   };
 
   return (
@@ -69,7 +91,10 @@ const SignUpScreen = () => {
           returnKeyType="next"
           label="Email Address"
           onSubmitEditing={onSubmitEmail}
-          onChangeText={onChangeEmail}
+          onChangeText={setLocalEmail}
+          error={errors.email}
+          onFocus={onFocusEmail}
+          value={localEmail}
         />
       </View>
       <View style={styles.input}>
@@ -86,7 +111,10 @@ const SignUpScreen = () => {
           maxLength={20}
           characterRestriction={20}
           onSubmitEditing={onSubmitPassword}
-          onChangeText={onChangePassword}
+          onChangeText={setLocalPassword}
+          error={errors.password}
+          onFocus={onFocusPassword}
+          value={localPassword}
         />
       </View>
       <View style={styles.input}>
@@ -102,8 +130,10 @@ const SignUpScreen = () => {
           title=""
           maxLength={20}
           characterRestriction={20}
-          onChangeText={onChangePasswordRetype}
-          error={error}
+          onChangeText={setLocalPasswordRetype}
+          error={errors.passwordRetype}
+          onFocus={onFocusPasswordRetype}
+          value={localPasswordRetype}
         />
       </View>
       <MaterialButton style={styles.button} onPress={onPress}>
